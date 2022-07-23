11d86487b095c77b7c5fcfa85fa339d3ccb6a002
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-07-23 22:25:15 +0300

    separate_file_form

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import Form from 'components/Form';
 
 export class App extends Component {
   state = {
     contacts: [],
-    name: '',
+  };
+
+  formSubmitHandler = data => {
+    const newContact = {
+      name: data.name,
+      number: data.number,
+      id: nanoid(),
+    };
+    this.setState(state => ({
+      contacts: [...state.contacts, newContact],
+    }));
   };
 
   render() {
+    const { contacts } = this.state;
+
     return (
       <div>
         <h1>Phonebook</h1>
-        <form>
-          <label htmlFor='name'>Name</label>
-        <input 
-          type="text"
-          name="name"
-          
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <button type="submit">Add contact</button>
-      </form>
-      <h2>Contacts</h2>
-      <ul>
-       {this.state.contacts.map(contact => (
-        <li key={nanoid()}>
-          {contact.name}
-        </li>
-       ))}
-      </ul>
+        <Form onFormSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+
+        <p>Find contacts by name</p> 
+        
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number} 
+            </li> 
+          ))}
+        </ul>
       </div>
     );
   }
